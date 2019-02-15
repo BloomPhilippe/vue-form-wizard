f05fb8fd51a553fd09dc860bb9a34936810ebb75
Philippe Bloom <philippe.bloom@produweb.be>
2019-02-15 08:12:42 +0100

    change config

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/projets/vue-form-wizard',
 
     /**
      * Source Maps
