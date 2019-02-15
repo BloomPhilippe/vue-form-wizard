e5dc411da43d16c19fe5e50c418a731a2e41fcaa
BLOOM Philippe <BLOOMP@isl.be>
2019-02-15 18:49:07 +0100

    translate

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Langues</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">FR</a>
+              <a class="dropdown-item" href="#">EN</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
